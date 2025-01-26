7da77960e3f0e5c6c36150626fee79b305fa127e
Denis <69577507+Desnn1ch@users.noreply.github.com>
2025-01-27 00:21:01 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   //Repo name
-  base: '/portfolio/',
+  base: '/Desnn1ch.com/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
